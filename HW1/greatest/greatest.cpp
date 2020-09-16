685eeef635a07bb02587b6235bc3c0310c239ca8
Trey Chiu <treychiu@outlook.com>
2020-09-15 16:59:53 -0800

    Created Lab04.cpp with code for iteration

---
+++ HW1/greatest/greatest.cpp
@@ -0,0 +1,21 @@
+/*
+* greatest.cpp
+* Trey Chiu
+* CS201
+* Sep 11, 2020
+* Outputs greatest positive integer of user inputted integers
+*/
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main()
+{
+	cout << "Enter a sequence of positive integers, ending with zero or a negative number. \n";
+	cout << "I will print the greatest positive integer entered.\n";
+
+	cin >> 
+
+}
---
+++ Labs/Lab04/Lab04.cpp
@@ -0,0 +1,20 @@
+/*
+* Lab04.cpp
+* Trey Chiu
+* CS201
+* Sep 7, 2020
+* Outputs numbers 1 - 100 with Fizz every multiple of 3
+*/
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	int i; // iterator
+	for (i = 1; i <= 100; i++)
+	{
+		cout << i << "\n";
+	}
+}
