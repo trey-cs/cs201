9b7b488f678d18330c5d6fbc3f4feb8597f5899e
Trey Chiu <treychiu@outlook.com>
2020-09-15 17:07:32 -0800

    Added "Fizz" code to Lab04

--- Labs/Lab04/Lab04.cpp
+++ Labs/Lab04/Lab04.cpp
@@ -2,19 +2,25 @@
 * Lab04.cpp
 * Trey Chiu
 * CS201
-* Sep 7, 2020
+* Sep 15, 2020
 * Outputs numbers 1 - 100 with Fizz every multiple of 3
 */
 
 #include <iostream>
 using std::cout;
-using std::endl;
 
 int main()
 {
 	int i; // iterator
 	for (i = 1; i <= 100; i++)
 	{
-		cout << i << "\n";
+		if (i % 3 == 0) // is number divisible by 3
+		{
+			cout << "Fizz" << "\n";
+		}
+		else
+		{
+			cout << i << "\n";
+		}
 	}
 }
