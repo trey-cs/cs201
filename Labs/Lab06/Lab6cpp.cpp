d80290d81bb3612e7621f55b2f17c5ad0f36f9a7
Trey Chiu <treychiu@outlook.com>
2020-09-16 11:20:33 -0800

    added Printstrsize function to Lab6.cpp

--- Labs/Lab06/Lab6cpp.cpp
+++ Labs/Lab06/Lab6cpp.cpp
@@ -1,14 +1,27 @@
 /*
-* filename.cpp
+* Lab6.cpp
 * Trey Chiu
 * CS201
-* Sep 7, 2020
-* purose
+* Sep 16, 2020
+* Function will print string and string length
 */
 
 #include <iostream>
+#include <string>
 using std::cout;
 using std::endl;
+using std::string;
 
+void Printstrsize(string str1)
+{
+	cout << "The string " << str1 << " is " << str1.size() << " bytes in length.\n";
+}
+
+// prints string and string length
 int main()
-{}
+{
+	string str1("baseketball");
+	Printstrsize(str1);
+	
+	return 0;
+}
