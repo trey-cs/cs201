07d6d6d4bb4690384d3b9d2f9ae61dc72082ffbc
Trey Chiu <treychiu@outlook.com>
2020-09-21 16:41:59 -0800

    Added scores.sln and scores.cpp to HW2 subd.

---
+++ HW/HW2/scores/scores.cpp
@@ -0,0 +1,19 @@
+/*
+* scores.cpp
+* Trey Chiu
+* CS201
+* Sep 21, 2020
+* 
+*/
+
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::getline;
+
+int main()
+{
+	
+}
