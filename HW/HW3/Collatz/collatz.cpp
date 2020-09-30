1bdc15fe8f5139a540c1006904417aef9536f92f
Trey Chiu <treychiu@outlook.com>
2020-09-29 22:32:30 -0800

    added while loop for collatz equation

--- HW/HW3/Collatz/collatz.cpp
+++ HW/HW3/Collatz/collatz.cpp
@@ -28,6 +28,20 @@ void getInt(int& int1)
 int main()
 {
 	int n;
-	getInt(n);
-	cout << n << endl;
+
+		getInt(n);
+		cout << n << " ";
+		while (n != 1)
+		{
+		
+			if (n % 2 == 0)
+			{
+				n = n / 2;
+			}
+			else
+			{ 
+				n = n*3 + 1;
+			}
+			cout << n << " ";
+		}
 }
